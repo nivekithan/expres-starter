10610785b2c56a67cc57f5a5bd633f9f66f27c6a
Nivekithan S <nivekithan363@gmail.com>
2022-06-03 10:16:01 +0530

    Some change

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ app.get("/", (req, res) => {
   res.send("Hello World!, Its 3000");
 });
 
-const port = 3000;
+const port = 4000;
 
 app.listen(port, () => {
   console.log(`App is running on Port : ${port}`);
